9308eca7f87c3507f9b75f353be1b3e6d7af8485
mastersunn <jbworth@protonmail.com>
2025-01-11 19:05:31 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+API_KEYS = {
+    "USGS": "PRIVATE",
+    "SOLANA_RPC": "https://api.mainnet-beta.solana.com"
+}
+
+DATA_PATHS = {
+    "raw_data": "src/data/raw",
+    "processed_data": "src/data/processed"
+}
+
+BLOCKCHAIN = {
+    "token_name": "VolcanAI",
+    "symbol": "VAI",
+    "decimals": 18
+}
